a7a80bbea60e9af67734d7184a0c14dcce71a355
kdmp23 <kemuelmercedes08@gmail.com>
2026-07-16 10:58:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -979,3 +979,34 @@ function saveInventory(){
     goBack();
 
 }
+
+function showToast(message){
+
+    const toast = document.createElement("div");
+
+    toast.className = "toast";
+
+    toast.textContent = message;
+
+    document.body.appendChild(toast);
+
+    setTimeout(() => {
+
+        toast.classList.add("show");
+
+    },10);
+
+    setTimeout(() => {
+
+        toast.classList.remove("show");
+
+        setTimeout(()=>{
+
+            toast.remove();
+
+        },300);
+
+    },1200);
+
+}
+
