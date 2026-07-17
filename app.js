4a37960c9fb2d8fb540ff2e857d219b20c3d4b32
kdmp23 <kemuelmercedes08@gmail.com>
2026-07-17 19:39:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 <script type="module">
+  
   // Import the functions you need from the SDKs you need
+  
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  
   const firebaseConfig = {
     apiKey: "AIzaSyDpDkjGsPG5Oa1lwaox6PHIKpmIvJ-bRxE",
     authDomain: "bunny-inventory.firebaseapp.com",
@@ -18,8 +23,11 @@
   };
 
   // Initialize Firebase
+  
   const app = initializeApp(firebaseConfig);
+  
   const analytics = getAnalytics(app);
+
 </script>
 
 const app = document.getElementById("app");
