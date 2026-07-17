61a120bf146a6a08e4e6fc96b9a44e2397948fd7
kdmp23 <kemuelmercedes08@gmail.com>
2026-07-17 19:45:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+
     apiKey: "AIzaSyDpDkjGsPG5Oa1lwaox6PHIKpmIvJ-bRxE",
+
     authDomain: "bunny-inventory.firebaseapp.com",
+
     projectId: "bunny-inventory",
+
     storageBucket: "bunny-inventory.firebasestorage.app",
+
     messagingSenderId: "1020869773891",
-    appId: "1:1020869773891:web:a374dc995cb0adda13a2f6",
-    measurementId: "G-1WSS8R0ESN"
-  };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+    appId: "1:1020869773891:web:a374dc995cb0adda13a2f6"
 
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
 
 const app = document.getElementById("app");
 
