47fe345e96dadf02bb90a5387e7f5d06aa367923
kdmp23 <kemuelmercedes08@gmail.com>
2026-07-17 20:02:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
 import {
-    getFirestore,
     collection,
     addDoc
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
